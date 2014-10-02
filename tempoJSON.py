d24caefaf7889be15ed6c982c9e14524ed53b1df
Carolyn Morris <carolyn@citilogics.com>
2014-10-02 13:44:04 -0400

    Update tempoJSON.py

--- tempoJSON.py
+++ tempoJSON.py
@@ -1,5 +1,6 @@
 # use Python 3.3.3
 
+import sqlite3
 import json
 import urllib.request
 import gzip
@@ -16,25 +17,51 @@ def getTimeFromLine(line):
     d = datetime(yyyy, mm, dd, h, m, s, 0)
     return(calendar.timegm(d.utctimetuple()))
 
-data=[]
-for i in range(2005, 2015):
-    i = str(i)
-    cincinnati = locationYear(724297, 93812, i)
-    # Get the data
-    with urllib.request.urlopen('ftp://ftp.ncdc.noaa.gov/pub/data/noaa//'+cincinnati) as file:
-        # Open the gzip file (in bytes mode)
-        with gzip.open(file, 'rb') as f:
-                datapoints = []
-                for line in f:
-                    time = getTimeFromLine(line)
-                    temp = float(line[87:92]) # pos 88-92 (offset by one due to Python 0 indexing)
-                    if temp != 9999: # filter missing data
-                        point = [time, temp]
-                        datapoints.append(point)
-                data = data + datapoints
+def parseWeatherData():
+    data=[]
+    for i in range(2005, 2015):
+        i = str(i)
+        cincinnati = locationYear(724297, 93812, i)
+        # Get the data
+        with urllib.request.urlopen('ftp://ftp.ncdc.noaa.gov/pub/data/noaa//'+cincinnati) as file:
+            # Open the gzip file (in bytes mode)
+            with gzip.open(file, 'rb') as f:
+                    datapoints = []
+                    for line in f:
+                        time = getTimeFromLine(line)
+                        temp = float(line[87:92]) # pos 88-92 (offset by one due to Python 0 indexing)
+                        if temp != 9999: # filter missing data
+                            point = [time, temp/10] # noaa temp is scaled by a factor of 10
+                            datapoints.append(point)
+                    data = data + datapoints
+    return data
+    # data is a list of [time, value] pairs of length 113,893 for range(2005, 2015)
 
-p = OrderedDict([('id', 'myFirstTimeSeries'), ('description', 'prototype time series for discussion'), ('columns', ['time', 'value']), ('units', 'MGD'), ('mean', 0.75), ('stdDev', 2.3)])
-d = OrderedDict([('type', 'series'), ('format', 'unixutc'), ('data', data), ('properties', p)])
+data = parseWeatherData()
 
-with open('/Users/carolyn/temperatures_cincinnati.json', 'w') as outfile:
-    json.dump(d, outfile, sort_keys=False, indent=2)
+def createTempoJSON(data): 
+    p = OrderedDict([('id', 'myFirstTimeSeries'), ('description', 'prototype time series for discussion'), ('columns', ['time', 'value']), ('units', 'MGD'), ('mean', 0.75), ('stdDev', 2.3)])
+    d = OrderedDict([('type', 'series'), ('format', 'unixutc'), ('data', data), ('properties', p)])
+    with open('/Users/carolyn/temperatures_cincinnati.json', 'w') as outfile:
+        json.dump(d, outfile, sort_keys=False, indent=2)
+
+def temperatureTable(data):
+    conn = sqlite3.connect("weather.db")
+    c = conn.cursor()
+
+    # meta table
+    c.execute('''CREATE TABLE IF NOT EXISTS 'meta' ('series_id' INTEGER PRIMARY KEY ASC AUTOINCREMENT, 'name' TEXT UNIQUE ON CONFLICT ABORT, 'units' TEXT, 'regular_period' INTEGER, 'regular_offset' INTEGER)''')
+
+    row = [['temperature', 'degrees Celsius']]
+    c.executemany('''INSERT OR IGNORE INTO meta (name, units) VALUES (?, ?)''', (row))
+    
+    # points table
+    c.execute('''CREATE TABLE IF NOT EXISTS 'temp' ('time' INTEGER, 'series_id' INTEGER REFERENCES 'meta'('series_id'), 'value' REAL, 'confidence' REAL, 'quality' INTEGER)''')
+    for i in data:
+        i = i+[1]+[0]+[0]
+        c.execute('''INSERT OR IGNORE INTO temp (time, value, series_id, quality, confidence) VALUES (?, ?, ?, ?,?)''', (i))
+
+    conn.commit()
+    conn.close()
+
+temperatureTable(data)
