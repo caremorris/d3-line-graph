f997ee5a99879b3dc2d5b8adf6f4cea2ad6c8523
Carolyn Morris <carolyn@citilogics.com>
2015-01-25 20:33:49 -0500

    Merge branch 'select-menu'
    Conflicts:
    	index.html
    	index.js
    	style.css

--- index.js
+++ index.js
@@ -1,80 +1,78 @@
-var data=[];
-
+// Set up chart
 var margin = {top: 20, right: 20, bottom: 30, left: 60},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
-    
+
+// Scales          
 var x = d3.time.scale()
     .range([0, width]);
-    
+            
 var y = d3.scale.linear()
     .range([height, 0]);
-    
+
+// Axes         
 var xAxis = d3.svg.axis()
     .scale(x)
     .orient("bottom");
-    
+            
 var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
-    
+            
 var line = d3.svg.line()    // returns a path generator; is both an object & a fxn
     .interpolate("basis")
-    .x(function(d) { return x(d.time); })
-    .y(function(d) { return y(d.value); });
-    
+    .x(function(d) { return x(d.time); }) // x is a function. it maps d.time to a pixel value between (0, width)
+    .y(function(d) { return y(d.value); }); // y is a function. it maps d.value to a pixel value between (0, height) :)
+            
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
-function render(data) {
+d3.json("data/cincyWeather1990.json", function(error, data) {
     data.forEach(function(d) {
         d.time = +d.time;
         d.time *= 1000;
         d.value = +d.value;
     });
-
-    // set the input domain
+    // set the input domain. min and max.
     x.domain(d3.extent(data, function(d) { return d.time; }));
     y.domain(d3.extent(data, function(d) { return d.value; }));
-
-    // y axis   
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
 
     // x axis 
     svg.append("g")         
         .attr("class", "x axis")
         .attr("transform", "translate(0, " + height + ")")
-        .call(xAxis);
-  
+        .call(xAxis);  
+
+    // y axis   
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Temperature (Fahrenheit)");
+    
+    // line
     svg.append("path")
         .data(data) 
         .attr("class", "line") 
         .attr("d", line(data));
-}
-
-d3.json("data/cincyWeather2000.json", function(error, data) {
-    render(data);
 });
 
-function randomYear() {
-    var year = 1990 + Math.round(Math.random() * 22);
-    var y = year.toString();
-    return y;
-}
 
-function update(data) {
+function update(data) {   
     data.forEach(function(d) {
         d.time = +d.time;
         d.time *= 1000;
         d.value = + d.value;
     });
 
-    x.domain(d3.extent(data, function(d) { return d.time; }) );
+    x.domain(d3.extent(data, function(d) { return d.time; }));
     y.domain(d3.extent(data, function(d) { return d.value; }));
 
     var svg = d3.select("body").transition();
@@ -90,10 +88,32 @@ function update(data) {
         .call(yAxis);
 }
 
-
-function load() {
-    var yr = randomYear();
-    d3.json("data/cincyWeather"+yr+".json", function(error, data) {
-        update(data);
+function load(name) {
+    d3.json("data/cincyWeather"+name+".json", function(error, json) {
+        update(json);
     });
+}
+
+// Dynamic select menu
+var select, options;
+d3.json("data/years.json", function(error, json) {
+    var data = json["data"];
+    
+    select = d3.select(".control-group")
+        .append("select")
+        .on("change", change);
+
+    options = select.selectAll("option").data(data);
+
+    options.enter()
+        .append("option")
+        .text(function(d) { return d.year; });
+    }
+);
+
+function change() {
+    var selectedIndex = select.property('selectedIndex'),
+        data          = options[0][selectedIndex].__data__;
+
+    load(data.year);
 }
